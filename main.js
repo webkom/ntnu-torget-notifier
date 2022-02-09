f5c048ac292b6a606ab98d0c60b99b303cc2d9c0
Peder Smith <smith.peder@gmail.com>
2022-02-09 16:46:13 +0100

    FixFix

--- main.js
+++ main.js
@@ -1,56 +1,70 @@
-const puppeteer = require("puppeteer");
-const slackNotify = require("slack-notify");
+const puppeteer = require('puppeteer');
+const slackNotify = require('slack-notify');
 
 const {
-  EACH_N_SEC = "60",
+  EACH_N_SEC = '60',
   SLACK_WEBHOOK_URL,
   FEIDE_USERNAME,
-  FEIDE_PASSWORD
+  FEIDE_PASSWORD,
 } = process.env;
 if (!SLACK_WEBHOOK_URL || !FEIDE_PASSWORD || !FEIDE_USERNAME) {
-  console.error("Env vars missing");
+  console.error('Env vars missing');
   return 2;
 }
 
 const slack = slackNotify(SLACK_WEBHOOK_URL);
 const seen = new Set();
 function sleep(ms) {
-  return new Promise(resolve => {
+  return new Promise((resolve) => {
     setTimeout(resolve, ms);
   });
 }
 let isFirst = true;
 (async () => {
   while (true) {
+    console.log('- Checking for new items');
     try {
       const browser = await puppeteer.launch({
         headless: true,
         devtools: false,
-        executablePath: "/usr/bin/chromium",
+        executablePath: '/usr/bin/chromium-browser',
         args: [
-          "--no-sandbox",
-          "--disable-setuid-sandbox",
-          "--disable-dev-shm-usage"
-        ]
+          '--no-sandbox',
+          '--disable-setuid-sandbox',
+          '--disable-dev-shm-usage',
+        ],
       });
       const page = await browser.newPage();
       page.setDefaultTimeout(60000);
-      await page.goto("https://www.ntnu.no/nettbutikk/gjenbruk/torget/", {
-        waitUntil: "load"
+      await page.goto('https://www.ntnu.no/nettbutikk/gjenbruk/torget/', {
+        waitUntil: 'load',
       });
-      await page.evaluate(() => {
-        document.querySelector('input[id="username"]').value = FEIDE_USERNAME;
-        document.querySelector('input[id="password"]').value = FEIDE_PASSWORD;
-        document.querySelector('button[type="submit"]').click();
-      });
-      await page.waitForSelector('main[role="main"]');
+
+      // Wait a bit so that the browser finishes executing JavaScript
+      await page.waitForTimeout(1000);
+
+      await page.waitForSelector('#content');
+
+      await page.evaluate(
+        ({ FEIDE_USERNAME, FEIDE_PASSWORD }) => {
+          document.querySelector('input[id="username"]').value = FEIDE_USERNAME;
+          document.querySelector('input[id="password"]').value = FEIDE_PASSWORD;
+          document.querySelector('button[type="submit"]').click();
+        },
+        { FEIDE_USERNAME, FEIDE_PASSWORD }
+      );
+
+      await page.waitForTimeout(3000);
+
+      await page.waitForSelector('main[id="main"]');
+
       const result = await page.evaluate(() => {
-        const things = document.getElementsByClassName("product");
+        const things = document.getElementsByClassName('product');
         const result = [];
         for (let thing of things) {
-          const id = thing.children[1].getAttribute("data-product_id");
-          const url = thing.children[0].getAttribute("href");
-          const img = thing.children[0].children[0].getAttribute("src");
+          const id = thing.children[1].getAttribute('data-product_id');
+          const url = thing.children[0].getAttribute('href');
+          const img = thing.children[0].children[0].getAttribute('src');
           const txt = thing.children[0].children[1].textContent;
           const txt2 = thing.children[0].children[2].textContent;
           result.push({ id, url, img, txt, txt2 });
@@ -59,43 +73,46 @@ let isFirst = true;
       });
 
       result.forEach(({ id, url, img, txt, txt2 }) => {
-        console.log("Found id", id);
+        console.log('- Found id', id);
         if (seen.has(id)) {
           console.log(`- Was already seen`);
           return;
         }
         seen.add(id);
+        console.log('- Added new item');
         if (isFirst) {
           return;
         }
 
-        console.log("- Notifying about it:", txt, txt2, id, url, img);
+        console.log('- Notifying about it:', txt, txt2, id, url, img);
         slack.send({
           text: txt,
           unfurl_links: 1,
           icon_url:
-            "https://innsida.ntnu.no/c/wiki/get_page_attachment?p_l_id=22780&nodeId=24647&title=Bruksregler+for+NTNU-logoen&fileName=logo_norsk_uten_slagord.jpg",
-          username: "NTNU",
+            'https://innsida.ntnu.no/c/wiki/get_page_attachment?p_l_id=22780&nodeId=24647&title=Bruksregler+for+NTNU-logoen&fileName=logo_norsk_uten_slagord.jpg',
+          username: 'NTNU',
           blocks: [
             {
-              type: "section",
-              block_id: "section567",
+              type: 'section',
+              block_id: 'section567',
               text: {
-                type: "mrkdwn",
-                text: `${txt} (${txt2}), ${url}`
+                type: 'mrkdwn',
+                text: `${txt} (${txt2}), ${url}`,
               },
               accessory: {
-                type: "image",
+                type: 'image',
                 image_url: img,
-                alt_text: txt
-              }
-            }
-          ]
+                alt_text: txt,
+              },
+            },
+          ],
         });
       });
       isFirst = false;
       browser.close();
-    } catch (e) {}
+    } catch (e) {
+      console.error('- Error: ', e);
+    }
     await sleep(parseInt(EACH_N_SEC, 10) * 1000);
   }
 })();
